b785077e14d02357e34d9c60f0015b00e17a57f9
Kennard Ng <kennard_ng@outlook.com>
2020-10-29 14:33:54 +0800

    hotfix swapping mechanics

--- train_ogo.py
+++ train_ogo.py
@@ -387,8 +387,11 @@ def train_ogo(epoch: int, train_loader, model, prediction_head: OddGraphOutPredi
         targets = targets.view(-1)
 
         # swap the graph with the negative graph; the negative graph can be at the same location.
-        swap_features = graph_features[:, targets, :]
-        graph_features[:, targets, :] = graph_features[:, -1, :]  # replace with negative graph
+        swap_idxs = targets + torch.arange(len(targets), device=torch.device(opt.device)) * num_graphs_per_batch
+        flattened_features = graph_features.view(num_total_graphs, opt.hidden_size)
+        swap_features = flattened_features[swap_idxs, :]
+        flattened_features[swap_idxs, :] = graph_features[:, -1, :]  # replace with negative graph
+        graph_features = flattened_features.view(batchsize, num_graphs_per_batch, opt.hidden_size)
         graph_features[:, -1, :] = swap_features  # replace with swapped graph
 
         # get the predictions

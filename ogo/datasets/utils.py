563362ef4de2566ff19384cea1f47eada9fcc0fe
Kennard Ng <kennard_ng@outlook.com>
2020-10-29 14:08:52 +0800

    add itertools chain

--- ogo/datasets/utils.py
+++ ogo/datasets/utils.py
@@ -5,7 +5,7 @@ import itertools
 def ogo_batcher():
     def collate_fn(batch: list):
         # each batch contains a list of graphs, so we merge the lists together to form a super list of graphs
-        graphs = itertools.chain(batch)
+        graphs = list(itertools.chain(batch))
         graphs = dgl.batch(graphs)
         return graphs
     return collate_fn
